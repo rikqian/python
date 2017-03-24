f690e29ab8e9396e0d5006917ba3b64ee3a914f9
qianqiang <rikqian@163.com>
2017-03-24 18:04:00 +0800

    generator

--- gen.py
+++ gen.py
@@ -1,4 +1,18 @@
 # 生成器
 
 longestline = max(len(line.strip()) for line in open('file.py'))
-print(longestline)
+# print(longestline)
+
+
+class A(object):
+
+    def __init__(self, i):
+        from time import sleep, time
+        sleep(i)
+        print(time())
+
+
+print([A(i) for i in range(5)])
+
+# for x in (A(i) for i in range(5)):
+#     print(x)
